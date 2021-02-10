ff1d811fc86602df5a0a791fd66851555178d03e
Fredrik Simonsson <simson@thesimson.net>
2021-02-11 00:21:06 +0900

    Chaning payload to transactions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 // extern crate serde;
 extern crate hmac_sha256;
 
+struct Transaction {
+    sender: String,
+    receiver: String,
+    amount: u128, // Unsigned reverse sender and receiver to reverse transactions
+}
+
+impl Transaction {
+
+    fn new<S:Into<String>,R:Into<String>>(s:S,r:R,amount:u128)->Self
+    {
+        Transaction{sender:s.into(),receiver:r.into(),amount}
+    }
+}
+
 struct Block {
-    payload: [u8; 4096],
+    payload: Vec<Transaction>,
     nonce: u64,
     seq: u64,
     prev_sha: [u8; 32],
@@ -16,12 +30,27 @@ trait Hashable {
     }
 }
 
+impl Hashable for Transaction {
+    fn bytes(&self) -> Vec<u8> {
+        let mut v = vec![];
+        v.extend(self.sender.as_bytes());
+        v.extend(self.receiver.as_bytes());
+        v.extend(self.amount.to_be_bytes().iter());
+        v
+    }
+}
+
 impl Hashable for Block {
     fn bytes(&self) -> Vec<u8> {
         let mut bytes = Vec::with_capacity(std::mem::size_of::<Block>());
 
         bytes.extend(self.nonce.to_be_bytes().iter());
-        bytes.extend(self.payload.iter());
+        bytes.extend(
+            self.payload
+                .iter()
+                .flat_map(|transaction| transaction.bytes())
+                .collect::<Vec<u8>>(),
+        );
         bytes.extend(self.prev_sha.iter());
         bytes.extend(self.seq.to_be_bytes().iter());
         // bytes.extend(self.nonce.to_be_bytes());
@@ -32,7 +61,7 @@ impl Hashable for Block {
 }
 
 impl Block {
-    fn new(payload: [u8; 4096], nonce: u64, seq: u64, prev_sha: [u8; 32]) -> Self {
+    fn new(payload: Vec<Transaction>, nonce: u64, seq: u64, prev_sha: [u8; 32]) -> Self {
         Block {
             payload,
             nonce,
@@ -44,7 +73,6 @@ impl Block {
     fn mine(&mut self, difficulty: u128) {
         use std::convert::TryInto;
         let mut d = u128::from_be_bytes((self.sha[0..16]).try_into().unwrap());
-        // while u128::from_be_bytes((self.sha[0..16]).try_into().unwrap()) < difficulty
         while d > difficulty {
             self.nonce += 7;
             let hash = self.hash();
@@ -55,8 +83,8 @@ impl Block {
 }
 fn main() {
     let mut v: Vec<Block> = vec![
-        Block::new([0; 4096], 0, 0, [0; 32]),
-        Block::new([0; 4096], 0, 1, [0; 32]),
+        Block::new(vec![Transaction::new("Alice", "Bob", 128),Transaction::new("Alice", "Eve", 28)], 0, 0, [0; 32]),
+        Block::new(vec![Transaction::new("Bob", "Eve", 108)], 0, 1, [0; 32]),
     ];
 
     let mut lasthash = [0; 32];
@@ -67,16 +95,18 @@ fn main() {
     }
     // verify the block chain
     let mut lasthash = [0; 32];
-    for mut block in v {
+    for block in v {
         let calculated = block.hash();
         println!(
             "Checking {} N={} {}",
             block.seq,
             block.nonce,
-            calculated == block.sha
+            calculated == block.sha &&
+            block.prev_sha == lasthash
         );
+        lasthash = calculated
     }
-    println!("Hello, world!");
+    println!("Done");
 }
 
 #[cfg(test)]
@@ -88,7 +118,7 @@ mod test {
     #[test]
     fn test_calculate_hash() {
         use super::Hashable;
-        let mut block = super::Block::new([0; 4096], 0, 0, [0; 32]);
+        let mut block = super::Block::new(vec![], 0, 0, [0; 32]);
         block.mine(0x00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
 
         // This is slower
