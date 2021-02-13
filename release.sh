0bdf311d6b192c8126af82d0263c4a5463fd4ba9
Fredrik Simonsson <simson@thesimson.net>
2021-02-14 07:40:33 +0900

    Updated release script

--- release.sh
+++ release.sh
@@ -2,10 +2,11 @@
 
 cargo fmt
 cargo test || exit -1
+cargo build || exit -1
 cargo doc || exit -1
 (cd target/doc; tar c *) | (cd docs ; tar x)
 cd docs
-git add `find blockchaindemo*`
+git add `find *`
 git commit -m "Auto update documentation"
 git push
 
