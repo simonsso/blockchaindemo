f2c131565200c37da8e51c36d1d70e5095abc3a6
Fredrik Simonsson <simson@thesimson.net>
2021-02-13 20:24:02 +0900

    Initial auto doc release

--- src/hashable.rs
+++
@@ -1,6 +0,0 @@
-pub trait Hashable {
-    fn bytes(&self) -> Vec<u8>;
-    fn hash(&self) -> [u8; 32] {
-        hmac_sha256::Hash::hash(&self.bytes())
-    }
-}
--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod block;
-pub use crate::block::Block;
-mod transaction;
-pub use crate::transaction::Transaction;
-mod hashable;
-pub use crate::hashable::Hashable;
-mod blockchain;
-pub use crate::blockchain::BlockChain;
-pub use crate::blockchain::BlockChainTrait;
