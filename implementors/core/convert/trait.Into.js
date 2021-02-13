ca3743618f009a72762be0fa975fc5eb50ac2d5d
Fredrik Simonsson <simson@thesimson.net>
2021-02-14 07:40:15 +0900

    Auto update documentation

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["rmp"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>&gt; for <a class=\"enum\" href=\"rmp/enum.Marker.html\" title=\"enum rmp::Marker\">Marker</a>","synthetic":false,"types":["rmp::marker::Marker"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
